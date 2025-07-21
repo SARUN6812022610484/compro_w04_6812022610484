54e3a98ce1b40864f4991b106522050ef6d21593
SARUN6812022610484 <s6812022610484@email.kmutnb.ac.th>
2025-07-21 15:03:46 +0700

    Add files via upload

---
+++ cp_pt_04-1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){ 
+    printf("HELLO , \nC Tokens!"); 
+    return 0 ;
+}
+//1. เป็น input 
+//2. int เป็น keyword,main เป็น Idnentifier
+//3. prinf เป็น Identifier, ข้อความ เป็น constant, \n เป็น operaor
+//4. return เป็น keyword, 0 เป็น constant 
