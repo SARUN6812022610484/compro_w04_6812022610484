94e86c3fa12dc8b58e1682ac1dded22dd26d1043
SARUN6812022610484 <s6812022610484@email.kmutnb.ac.th>
2025-07-23 18:15:04 +0700

    First commit

---
+++ coding_w04-01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (){
+    printf("hello, \nC Tokens !");
+    return 0 ;
+}
+//1. เป็น input 
+//3. int เป็น keyword,main เป็น Idnentifier
+//4. printf เป็น Identifier, ข้อความ เป็น constant, \n เป็น operaor
+//5. return เป็น keyword, 0 เป็น constant 
